827fa096b0739000202c3f5ff9a4b9e3d1566944
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-05 22:41:20 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,68 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int sum = 0;
+	bool flag = true;
+	
+	for (int i = 0; i < strlen(str) + 1; i++)
+	{
+		flag = true;
+		if ((str[i - 1] == ' ' || i == 0) and (str[i] != ' ' and str[i]!='\0'))
+		{
+			while (str[i] != ' ' and str[i] != '\0')
+			{
+				if (str[i] >= '0' && str[i] <= '9')
+					flag = false;
+				i++;
+			}
+			if (flag)
+				sum++;
+		}
+	}
+	return sum;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    int sum = 0;
+	bool flag = true;
+	
+	for (int i = 0; i < strlen(str) + 1; i++)
+	{
+		flag = true;
+		if ((str[i - 1] == ' ' || i == 0) and (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			i++;
+			while (str[i] != ' ' and str[i] != '\0')
+			{
+				if (!(str[i] >= 'a' && str[i] <= 'z'))
+					flag = false;
+				i++;
+			}
+			if (flag)
+				sum++;
+		}
+	}
+	return sum;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    double sum_word = 0;
+	double count_letter = 0;
+	bool flag = true;
+	
+	for (int i = 0; i < strlen(str) + 1; i++)
+	{
+		flag = true;
+		if ((str[i - 1] == ' ' || i == 0) and (str[i] != ' ' and str[i] != '\0'))
+		{
+			
+			while (str[i] != ' ' and str[i] != '\0')
+			{
+				count_letter++;
+				i++;
+			}
+			sum_word++;
+		}
+	}
+	return round(count_letter/sum_word);
 }
