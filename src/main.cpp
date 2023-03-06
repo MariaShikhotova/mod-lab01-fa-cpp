6c32fdcf3368b30b690523c141b8ace70816d323
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-06 20:07:45 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,5 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
-#include <iostream>
-using namespace std;
 int main() {
-    char a[100];
-	cin.getline(a, 100);
-	char* s = a;
-	cout << faStr1(s) << "\n";
-	cout << faStr2(s) << "\n";
-	cout << faStr3(s) << "\n";
+    return 0;
 }
