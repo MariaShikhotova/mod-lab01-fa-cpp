86d3337ada2915d9894d94af4559824d69f1a466
MariaShikhotova <126818543+MariaShikhotova@users.noreply.github.com>
2023-03-06 20:54:51 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -5,7 +5,7 @@ unsigned int faStr1(const char *str) {
 	bool flag = true;
 	for (int i = 0; i < strlen(str) + 1; i++) {
 		flag = true;
-		if ((str[i - 1] == ' ' || i == 0) && (str[i] != ' ' && str[i] != '\0')) {
+		if ( (str[i - 1] == ' ' || i == 0) && (str[i] != ' ' && str[i] != '\0') ) {
 			while (str[i] != ' ' && str[i] != '\0') {
 				if (str[i] >= '0' && str[i] <= '9') {
 					flag = false;
